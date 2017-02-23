0a1946a3f9d08ff57122e12bc0df73ed73f07968
Kévin Guiot <contact@kevinguiot.fr>
2017-02-23 19:24:32 +0100

    Ajout d'une sécurité qui permet d'éviter les requêtes à l'A.P.I Twitter si toutes les informations de connexion ne sont pas mentionnées.

--- login.php
+++ login.php
@@ -15,57 +15,75 @@ if(!isConnectToTwitter()) {
             ${$key} = $post;
         }
         
-        // On se connecte à Twitter avec les informations mentionnées
-        $twitter = new Twitter(
-            $consumerKey,
-            $consumerSecret,
-            $accessToken,
-            $accessTokenSecret
-        );
+        // Si toutes les informations de connexion sont mentionnées
+        if(
+           !empty($consumerKey) &&
+           !empty($consumerSecret) &&
+           !empty($accessToken) &&
+           !empty($accessTokenSecret)
+        ) {
         
-        // On effectue un test de récupération d'informations
-        try {
+            // On se connecte à Twitter avec les informations mentionnées
+            $twitter = new Twitter(
+                $consumerKey,
+                $consumerSecret,
+                $accessToken,
+                $accessTokenSecret
+            );
             
-            //On récupère les tweets qui contiennent le hashtag #php
-            $results = $twitter->search('#php');
-            
-            // On parcourt le nom des cookies
-            foreach($cookies as $cookie) {
+            // On effectue un test de récupération d'informations
+            try {
                 
-                // On enregistre le cookie
-                setcookie($cookie, ${$cookie});                
+                //On récupère les tweets qui contiennent le hashtag #php
+                $results = $twitter->search('#php');
+                
+                // On parcourt le nom des cookies
+                foreach($cookies as $cookie) {
+                    
+                    // On enregistre le cookie
+                    setcookie($cookie, ${$cookie});
+                }
+                
+                // On redirige l'utilisateur
+                header('location: index.php');
+                exit;
+            
+            } catch (Exception $e) {
+                
+                //On affiche l'erreur renvoyée.
+                echo 'Une erreur s\'est produite: <br><b>',  $e->getMessage(), "</b>";
             }
         
-        } catch (Exception $e) {
-            echo 'Une erreur s\'est produite: <br><b>',  $e->getMessage(), "</b>";
+        } else {
+            echo "<b>Veuillez remplir toutes les informations.</b>";
         }
         
-    } else {
-        
-        // On affiche le formulaire
-        ?>
-        
-        <form method="post">
-            <table>
-                <?php
-                //On parcourt le nom des cookies
-                foreach($cookies as $cookie) {
-                    echo '<tr>';
-                    echo '<td>'.$cookie.'</td>';
-                    echo '<td><input type="text" name="'.$cookie.'"></td>';
-                    echo '</tr>';
-                }
-                ?>
-                
-                <tr>
-                    <td colspan="2">
-                        <input type="submit" value="Se connecter à l'A.P.I Twitter">
-                    </td>
-                </tr>
-            </table>
-        </form>
-        
-        <?php
+        echo '<hr>';
     }
+    // On affiche le formulaire
+    ?>
+    
+    <p style="font-weight:bold;">Veuillez saisir vos identifiants de connexion à l'A.P.I Twitter.</p>
+    
+    <form method="post">
+        
+        <table border="1">
+            <?php
+            //On parcourt le nom des cookies
+            foreach($cookies as $cookie) {
+                echo '<tr>';
+                echo '<td>'.$cookie.'</td>';
+                echo '<td><input type="text" name="'.$cookie.'"></td>';
+                echo '</tr>';
+            }
+            ?>
+        </table>
+        
+        <hr>
+        
+        <input type="submit" value="Se connecter à l'A.P.I Twitter">
+    </form>
+    
+    <?php
 }
 ?>
