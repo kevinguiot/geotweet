57bbbbd0b9a4c1c8f49f368adfd493bbdfdcd487
Kévin Guiot <contact@kevinguiot.fr>
2017-02-23 19:42:32 +0100

    Améliorations des fonctions
    - Ajout de la fonction testConnexionTwitter() permettant de savoir si la connexion est bonne ou pas.
    - Amélioration de la fonction isConnectToTwitter() => appel la fonction testConnexionTwitter() à chaque appel.

--- include/functions.php
+++ include/functions.php
@@ -3,7 +3,7 @@
 /**
  * Permet de savoir si l'utilisateur est connecté à l'A.P.I Twitter
  *
- * @return array cookies
+ * @return bool État de connexion à Twitter
  */
 function isConnectToTwitter() {
     
@@ -23,7 +23,43 @@ function isConnectToTwitter() {
         }
     }
     
+    // On se connecte à Twitter avec les informations mentionnées
+    $twitter = new Twitter(
+        $consumerKey,
+        $consumerSecret,
+        $accessToken,
+        $accessTokenSecret
+    );
+
+    // On regarde si la connexion est bonne avec Twitter
+    $testConnexionTwitter = testConnexionTwitter($twitter);
+    
     // On retourne la réponse
-    return $isConnect;
+    return $testConnexionTwitter;
+}
+
+/**
+ * Permet de savoir si les informations de connexion à Twitter sont bonnes
+ *
+ * @param twitter => instance de la classe Twitter
+ * 
+ * @return bool État de connexion à Twitter
+ */
+function testConnexionTwitter( $twitter) {
+    
+    // On teste la connexion avec Twitter
+    try {
+        
+        //On récupère les tweets qui contiennent le hashtag #php
+        $results = $twitter->search('#php');
+        
+        // On définit la connexion comme réussie
+        $isConnect = true;
+        
+    } catch (Exception $e) {
+        $isConnect = false;
+    }
+ 
+    return $isConnect;    
 }
 ?>
